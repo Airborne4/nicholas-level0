f94cd47153633279dd7220d300e634811b479750
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-01-27 16:57:44 -0800

    gsgdsghsfh
    fdzhdzhxzhhxhsdhzc

---
+++ src/DoyoulikePIZZA.java
@@ -0,0 +1,50 @@
+import javax.swing.JOptionPane;
+
+import jdk.nashorn.internal.scripts.JO;
+
+public class DoyoulikePIZZA {
+public static void main(String[] args) {
+	String pizza =JOptionPane.showInputDialog("Do you like Pizza???");
+//Start of YES
+	if (pizza.equals("yes")) {
+String pep=JOptionPane.showInputDialog("Do ya lik pep ma dude?");
+if (pep.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Enjoy yo pep pizza m8!");
+}
+	
+	else{
+		 JOptionPane.showMessageDialog(null, "Enjoy yo cheese pizza m8!");
+	}
+	}
+if (pizza.equals("no")){
+	String salad=JOptionPane.showInputDialog("Do ya lik sladddd m8???");
+
+if (salad.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Enjoy yo pep salad.");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Yo you be hungies m8");
+}
+}
+
+
+
+
+
+
+
+if (pizza.equals("no")) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ src/ForLoops.java
@@ -0,0 +1,16 @@
+
+public class ForLoops {
+public static void main(String[] args) {
+	for (int i = 0; i < 500; i++) {
+		System.out.print(i+1 + " ");
+		if (i%2==1) {
+			System.out.println("even");
+			
+		}
+		else{
+			System.out.println("odd");
+		}
+	}
+	
+}
+}
