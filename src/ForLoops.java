ec4d46e0caf212316a92ff9d33fbbea32b97d383
najclark <league@WTS7.attlocal.net>
2017-02-03 17:00:10 -0800

    i865kl846o83
    75i753oi864o46o64

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,15 +1,8 @@
 
 public class ForLoops {
 public static void main(String[] args) {
-	for (int i = 0; i < 500; i++) {
-		System.out.print(i+1 + " ");
-		if (i%2==1) {
-			System.out.println("even");
-			
-		}
-		else{
-			System.out.println("odd");
-		}
+	for (int i = 0; i < 784; i+=7) {
+		System.out.print(i + " ");
 	}
 	
 }
